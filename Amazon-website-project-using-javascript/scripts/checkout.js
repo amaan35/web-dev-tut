3b7efbb9f7956ec65fa4640ba37c1e6ac2d7961e
amaan35 <amaanlm35@gmail.com>
2023-11-27 23:42:09 +0530

    updating HTML when delete button is clicked

--- Amazon-website-project-using-javascript/scripts/checkout.js
+++ Amazon-website-project-using-javascript/scripts/checkout.js
@@ -15,7 +15,7 @@ cart.forEach((cartItem)=>{
 
     cartSummaryHTML = cartSummaryHTML + 
     `
-    <div class="cart-item-container">
+    <div class="cart-item-container js-cart-item-container-${matchingProduct.id}">
         <div class="delivery-date">
         Delivery date: Tuesday, June 21
         </div>
@@ -97,6 +97,10 @@ cart.forEach((cartItem)=>{
     link.addEventListener('click', ()=>{
         const productId = link.dataset.productId;
         removeFromCart(productId);
+
+        const container = document.querySelector(`.js-cart-item-container-${productId}`);
+
+        container.remove();
     });
   });
 });
