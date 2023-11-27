d66779085922b026396efe60d0ffbece5d6dd781
amaan35 <amaanlm35@gmail.com>
2023-11-27 20:27:34 +0530

    making the cart quantity interactive

--- Amazon-website-project-using-javascript/scripts/amazon.js
+++ Amazon-website-project-using-javascript/scripts/amazon.js
@@ -74,6 +74,12 @@ document.querySelectorAll('.js-add-to-cart')
                 quantity: 1
             });
         }
-        console.log(cart);
+
+        let cartQuantity = 0;
+        cart.forEach((item)=>{
+            cartQuantity = cartQuantity + item.quantity;
+        });
+
+        document.querySelector('.js-cart-quantity').innerHTML = cartQuantity;
     });
 });
