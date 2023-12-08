8e99d43a2c0c54447253aaacfcf8b943965fac37
amaan35 <amaanlm35@gmail.com>
2023-12-08 15:34:59 +0530

    Learning about functional and class components

---
+++ ReactJS tutorial/functionalcomponents/src/components/Greet.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+// function Greet(){
+//     return <h1>Hello Syed Amaan Ali</h1>
+// }
+
+const Greet = () => <h1>Hello Syed Amaan Ali</h1>
+//Adding default keyword allows us to import this function with any name in App.js
+export default Greet
