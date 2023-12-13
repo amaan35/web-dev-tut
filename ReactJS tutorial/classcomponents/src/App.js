64cdb9894ed156087fd823d4c1a95c95f3979310
amaan35 <amaanlm35@gmail.com>
2023-12-14 04:35:07 +0530

    learning more about class components, props, states, class fields, componentDidMount, componentDidUpdate, componentWillUnmount

--- ReactJS tutorial/classcomponents/src/App.js
+++ ReactJS tutorial/classcomponents/src/App.js
@@ -1,13 +1,61 @@
 import logo from './logo.svg';
 import './App.css';
 import Welcome from './components/Welcome';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <Welcome/>
-    </div>
-  );
+class App extends React.Component {
+  // constructor(props){
+  //   super(props);
+  //   this.state = {
+  //     counter : 0
+  //   }
+  // }
+
+  // increment(){
+  //   this.setState((prevState)=>({counter: prevState.counter+1}))
+  // }
+
+  state = {
+    name: 'Amaan',
+    count: 0,
+    show: true
+  }
+  inc = ()=>{
+    this.setState((prevState)=>({count: prevState.count+1}))
+  }
+
+  componentDidMount(){
+    console.log("component created")
+    this.setState({count:10})
+  }
+
+  componentDidUpdate(){
+    console.log("component rendered")
+    //if condition is not given, it will fall into an infinite loop
+    if(this.state.count < 50){
+      this.setState((prevState)=>({count: prevState.count+10}))
+    }
+  }
+
+  render(){
+    let welcome;
+    if(this.state.show){
+      welcome = <Welcome/>
+    }
+    return (
+      <div className="App">
+        {/**Passing props to another component */}
+        <Welcome name={'Syed Amaan Ali'}/>
+        <h3>{this.state.name}</h3>
+        <h3>{this.state.count}</h3>
+        <button onClick={this.inc}>increment</button>
+        {welcome}
+        <button onClick={()=>this.setState({show:false})}>Remove above class component</button>
+        {/* <h3>{this.state.counter}</h3>
+        <button onClick={this.increment}>increment counter</button> */}
+      </div>
+    );
+  }
 }
 
 export default App;
--- ReactJS tutorial/classcomponents/src/components/Welcome.js
+++ ReactJS tutorial/classcomponents/src/components/Welcome.js
@@ -1,7 +1,15 @@
 import React, {Component} from 'react'
 class Welcome extends Component{
+    componentWillUnmount(){
+        alert("Class component will unmount")
+    }
     render(){
-        return <h1>Class component</h1>
+        return(
+            <>
+                <h1>Class component</h1>
+                <h2>{this.props.name}</h2>
+            </>
+        )
     }
 }
 export default Welcome
